dde4842dd35c050dece12b3588b069765ac980ce
HNebot <nebot.hector@gmail.com>
2015-04-21 16:56:36 +0200

    Creacion de test

---
+++ PracticaProgramacionAvanzada/src/test/TarifaTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TarifaTest {
+
+	@Test
+	public void testGetTarifa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNombreTarifa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCosteLlamada() {
+		fail("Not yet implemented");
+	}
+
+}
